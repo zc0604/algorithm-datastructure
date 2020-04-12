53d6bcffdffc8ddc16ae766810d486696d7d9913
cong_zhang <cong_zhang@idgcapital.com>
2020-04-12 18:20:10 +0800

    二分查找

---
+++ src/main/java/abc/geettime/alg/dimension/sort/BinarySort.java
@@ -0,0 +1,36 @@
+package abc.geettime.alg.dimension.sort;
+
+/**
+ * @Description 二分查找
+ * @auther cong zhang
+ * @email zc.bjut@gmail.com
+ * @create 2020-04-12 18:05
+ */
+public class BinarySort {
+
+    public static void main(String[] args) {
+        BinarySort binarySort = new BinarySort();
+        int num [] = {1,4,5,6,7,8,9,11};
+        System.out.println(binarySort.binarysort(num, 11));
+    }
+
+    public int binarysort(int []num, int key){
+        int low = 0 ;
+        int high = num.length - 1;
+
+        while( low <= high){
+            int mid = (low+ high)/2 ;
+            if( num[mid] > key){
+                high = high - 1;
+            }else if( num[mid] < key){
+                low = low + 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1 ;
+    }
+
+
+
+}
